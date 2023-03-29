ba80bfc3d9d596cc9ec96bb82240bfec567717d1
CodeDux <kristoffer.code@gmail.com>
2023-03-29 19:58:06 +0200

    move noopstorage to subfolder

--- src/storage.rs
+++ src/storage.rs
@@ -1,3 +1,5 @@
+mod noopstorage;
+pub use noopstorage::*;
 use std::{
     collections::HashMap,
     fs::File,
@@ -97,21 +99,5 @@ impl Storage for JsonStorage {
         }
 
         println!("Loaded {entries_count} events");
-    }
-}
-
-/// This does nothing with commands, truly in-memory mode with no persistance state
-pub struct NoopStorage;
-
-impl Storage for NoopStorage {
-    fn prepare_command<'de, TModel, T: Command<'de, TModel>>(&mut self, _command: &T) {}
-
-    fn commit_command(&mut self) {}
-
-    fn restore<TModel>(
-        &mut self,
-        _model: &mut TModel,
-        _commands: &HashMap<String, Box<dyn Fn(&[u8], &mut TModel) -> ()>>,
-    ) {
     }
 }
---
+++ src/storage/noopstorage.rs
@@ -0,0 +1,19 @@
+use super::Storage;
+use crate::Command;
+use std::collections::HashMap;
+
+/// This does nothing with commands, truly in-memory mode with no persistance state
+pub struct NoopStorage;
+
+impl Storage for NoopStorage {
+    fn prepare_command<'de, TModel, T: Command<'de, TModel>>(&mut self, _command: &T) {}
+
+    fn commit_command(&mut self) {}
+
+    fn restore<TModel>(
+        &mut self,
+        _model: &mut TModel,
+        _commands: &HashMap<String, Box<dyn Fn(&[u8], &mut TModel) -> ()>>,
+    ) {
+    }
+}
